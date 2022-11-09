96fdda913bf7717185fa692a5eae54ba904ecaa0
rorols78 <81967051+rorols78@users.noreply.github.com>
2022-11-09 17:31:59 +0100

    Component

---
+++ pokemon-app/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {POKEMONS} from "./mock-pokemon-list";
+import {Pokemon} from "./pokemon";
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Liste de Pokémons</h1>`,
+})
+export class AppComponent implements OnInit{
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[0]);
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  }
+}
