cd3e99f8350ef6fc631585cc3fec61de64e3a32d
rorols78 <81967051+rorols78@users.noreply.github.com>
2022-11-15 17:26:20 +0100

    update Template

--- pokemon-app/src/app/app.component.ts
+++ pokemon-app/src/app/app.component.ts
@@ -4,17 +4,25 @@ import {Pokemon} from "./pokemon";
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Liste de Pokémons</h1>`,
+  templateUrl: `app.component.html`,
 })
 export class AppComponent implements OnInit{
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
 
   ngOnInit() {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    if(pokemon){
+      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Vous avez demandé un pokémon qui n'existe pas.`);
+      this.pokemonSelected = pokemon;
+    }
+
   }
 }
